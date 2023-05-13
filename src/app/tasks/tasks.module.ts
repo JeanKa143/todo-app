6cae42edc927228850b79255f8c2ac040684ab47
Jean Carlo Jimenez <jeanjimenez143.dev@outlook.com>
2023-05-12 22:23:30 -0600

    🚧 Add add-task component

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -5,9 +5,10 @@ import { TasksRoutingModule } from './tasks-routing.module';
 import { TaskListPage } from './pages/task-list/task-list.page';
 import { SharedModule } from '../shared/shared.module';
 import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
 @NgModule({
-  declarations: [TaskListPage, LoadingSpinnerComponent],
+  declarations: [TaskListPage, LoadingSpinnerComponent, AddTaskComponent],
   imports: [CommonModule, TasksRoutingModule, SharedModule]
 })
 export class TasksModule {}
